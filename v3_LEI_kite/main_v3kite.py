d33884f080364e2e829d9b93d6fe435f1ab2e143
Oriol Cayon <ocayon@tudelft.nl>
2024-04-17 11:04:37 +0200

    changed files location for clarity

--- v3_LEI_kite/main_v3kite.py
+++ v3_LEI_kite/main_v3kite.py
@@ -19,16 +19,16 @@ ring_geo = '5fil' # System of vorticity defined with 5 or 3 filaments per wing s
 model = 'VSM'     # Choose between Vortex Step Method (VSM) or Lifting Line Method (LLT)
 # Convergence criteria
 conv_crit = {
-    'Niterations': 1500,
+    'Niterations': 1000,
     'error' : 1e-5,
-    'Conv_weight': 0.03
+    'Relax_factor': 0.03
     }
 
 # Wind speed vector definition  
 Umag = 22               # Wind speed magnitude
 aoa = 12*np.pi/180      # Angle of attack
-sideslip = 0/180*np.pi  # Sideslip angle
-Uinf = np.array([np.cos(aoa)*np.cos(sideslip),np.sin(sideslip),np.sin(aoa)])*Umag # Wind speed vector
+ss = 0/180*np.pi  # ss angle
+Uinf = np.array([np.cos(aoa)*np.cos(ss),np.sin(ss),np.sin(aoa)])*Umag # Wind speed vector
 
 # Definition of the geometry
 Atot = 19.753                    # Projected area 
@@ -100,16 +100,121 @@ plt.ylabel(r'Thickness ratio ($t/c$)')
 plt.xlabel(r'$y$ [m]')
 
 #%% SOLVER  
-# Define system of vorticity
-controlpoints,rings,bladepanels,ringvec,coord_L = VSM.create_geometry_LEI(
-    coord, Uinf, N,ring_geo,model)
-# Solve for Gamma
-Fmag, Gamma,aero_coeffs = VSM.solve_lifting_line_system_matrix_approach_semiinfinite(
-    ringvec, controlpoints, rings,Uinf,data_airf,conv_crit,model)
-#%OUTPUT Results
-F_rel,F_gl,Ltot,Dtot,CL,CD = VSM.output_results(Fmag,aero_coeffs,ringvec,Uinf,controlpoints,Atot)
 
-print(CL,CD)
+alphas = np.arange(-10,30,2)/180*np.pi #[10*np.pi/180] 
+sideslips =[0/180*np.pi]#np.arange(0,15,2)/180*np.pi#
+
+ss_map = []
+aoa_map = []
+CL = []
+CD = []
+CS = []
+for aoa in alphas:
+    for ss in sideslips:
+        print(aoa,ss)
+        Gamma0 = np.zeros(N-1)
+        Uinf = np.array([np.cos(aoa)*np.cos(ss),np.sin(ss),np.sin(aoa)])*Umag # Wind speed vector
+        # Define system of vorticity
+        controlpoints,rings,bladepanels,ringvec,coord_L = VSM.create_geometry_LEI(
+            coord, Uinf, N,ring_geo,model)
+        # Solve for Gamma
+        Fmag, Gamma,aero_coeffs = VSM.solve_lifting_line_system_matrix_approach_semiinfinite(
+            ringvec, controlpoints, rings,Uinf,Gamma0,data_airf,conv_crit,model)
+        #%OUTPUT Results
+        F_rel,F_gl,Ltot,Dtot,CLi,CDi,CSi = VSM.output_results(Fmag,aero_coeffs,ringvec,Uinf,controlpoints,Atot)
+        aoa_map.append(aoa)
+        ss_map.append(ss)
+        CL.append(CLi)
+        CD.append(CDi)
+        CS.append(CSi)
+#%%
+import pandas as pd
+results = np.vstack((CL,CD,alphas*180/np.pi))
+column_names = ['CL','CD','aoa']
+df = pd.DataFrame(data=results.T, columns=column_names)
+
+df.to_csv('VSM_data.csv', index=False)
+#%%
+CL_struts = np.loadtxt('RANS_CL_alpha_struts.csv',delimiter = ',') 
+CD_struts = np.loadtxt('RANS_CD_alpha_struts.csv',delimiter = ',') 
+
+CL_CFD = CL_struts[:,1]
+aoas_CFD = CL_struts[:,0]
+CD_CFD = np.interp(aoas_CFD,CD_struts[:,0],CD_struts[:,1])
+results = np.vstack((CL_CFD,CD_CFD,aoas_CFD))
+column_names = ['CL','CD','aoa']
+df1 = pd.DataFrame(data=results.T, columns=column_names)
+df1.to_csv('CFD_data.csv', index=False)
+#%%
+
+ss_map = np.array(ss_map)
+
+CL = np.array(CL)
+CD = np.array(CD)
+CS = np.array(CS)
+plt.figure()
+plt.scatter(np.degrees(ss_map),CL**3/CD**2)
+plt.plot
+plt.figure()
+plt.scatter(np.degrees(aoa_map),CL**3/CD**2)
+plt.plot
+
+
+
+# plt.figure()
+# plt.plot(np.degrees(sideslips),CL**3/CD**2)
+# plt.xlabel('ss')
+# plt.ylabel('pow_factor')
+# plt.grid()
+
+# plt.figure()
+# plt.plot(np.degrees(sideslips),CL)
+# plt.xlabel('CL')
+# plt.ylabel('ss')
+# plt.grid()
+
+# plt.figure()
+# plt.plot(np.degrees(sideslips),CD)
+# plt.xlabel('alpha')
+# plt.ylabel('CD')
+# plt.grid()
+
+# plt.figure()
+# plt.plot(np.degrees(sideslips),CL/CD)
+# plt.xlabel('alpha')
+# plt.ylabel('CL/CD')
+# plt.grid()
+
+
+CL = np.array(CL)
+CD = np.array(CD)
+CS = np.array(CS)
+
+plt.figure()
+plt.plot(np.degrees(alphas),CL**3/CD**2)
+plt.xlabel('alpha')
+plt.ylabel('pow_factor')
+plt.grid()
+
+plt.figure()
+plt.plot(np.degrees(alphas),CL)
+plt.xlabel('alpha')
+plt.ylabel('CL')
+plt.grid()
+
+plt.figure()
+plt.plot(np.degrees(alphas),CD)
+plt.xlabel('alpha')
+plt.ylabel('CD')
+plt.grid()
+
+plt.figure()
+plt.plot(np.degrees(alphas),CL/CD)
+plt.xlabel('alpha')
+plt.ylabel('CL/CD')
+plt.grid()
+
+
 #%% PLOTS
 
 fig = plt.figure(figsize = (6,5))
