5dcd5b654be27f1810965bdb9b7b09aa1b768234
ocayon <94445351+ocayon@users.noreply.github.com>
2024-06-20 15:01:36 +0200

    Add Filaments class

--- src/VSM/HorshoeVortex.py
+++ src/VSM/HorshoeVortex.py
@@ -1,3 +1,5 @@
+from  abc import ABC
+
 class HorshoeVortex:
     """
     A class to represent a horshoe vortex.
@@ -11,22 +13,76 @@ class HorshoeVortex:
 
     """
 
-    def __init__(self, LE1, TE1, LE2, TE2, aerodynamic_center = 0.25):
+    def __init__(self, LE_point_1, TE_point_1, LE_point_2, TE_point_2, aerodynamic_center = 0.25):
         
-        bound_point_1 = LE1*(1-aerodynamic_center)+TE1*aerodynamic_center
-        bound_point_2 =         
-        self.filaments.append(Filament())
+        self.filaments = []
+        bound_point_1 = LE_point_1*(1-aerodynamic_center)+TE_point_1*aerodynamic_center
+        bound_point_2 = LE_point_2*(1-aerodynamic_center)+TE_point_2*aerodynamic_center      
+        self.filaments.append(BoundFilament(x1 = bound_point_1, x2 = bound_point_2))
+        self.filaments.append(BoundFilament(x1 = TE_point_1, x2 = bound_point_1))
+        self.filaments.append(BoundFilament(x1 = bound_point_2, x2 = TE_point_2))
 
     
 
-    def update_for_va(self, va: list):
+    def get_velocity_induced_bound_2D(self, control_point):
+        """"
+        This function calculates the 2D induced velocity at the control point due to the bound vortex filaments
         """
-        Updates the horshoe vortex for the given va.
+        pass
+    
 
-        Args:
-            va (list): List of va values.
+class Filament(ABC):
+    """
+    A class to represent a filament.
 
-        Returns:
-            None
-        """
-        return self.horshoe_vortex
+    input:
+    two points defining the filament
+
+    output:
+    a filament object
+
+    """
+
+    def __init__(self):
+        pass
+
+    def calculate_induced_velocity(self, point):
+        pass
+
+class BoundFilament:
+    """
+    A class to represent a filament.
+
+    input:
+    two points defining the filament
+
+    output:
+    a filament object
+
+    """
+
+    def __init__(self, x1, x2):
+        self.x1 = x1
+        self.x2 = x2
+    
+    def calculate_induced_velocity(self, point):
+        pass
+
+class InfiniteFilament:
+    """
+    A class to represent a filament.
+
+    input:
+    two points defining the filament
+
+    output:
+    a filament object
+
+    """
+
+    def __init__(self, x1, direction):
+        self.x1 = x1
+        self.direction = direction
+    
+    def calculate_induced_velocity(self, point):
+        pass
