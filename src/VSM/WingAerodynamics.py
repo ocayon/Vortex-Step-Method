Correct calculation of projected are in src and tests

src/VSM/WingAerodynamics.py
@@ -470,7 +470,6 @@ class WingAerodynamics:
             dir_lift_prescribed_va = dir_lift_prescribed_va / np.linalg.norm(
                 dir_lift_prescribed_va
             )
-            print(spanwise_direction)
             lift_prescribed_va = np.dot(
                 lift_induced_va, dir_lift_prescribed_va
             ) + np.dot(drag_induced_va, dir_lift_prescribed_va)
@@ -534,7 +533,9 @@ class WingAerodynamics:
             fx_global += fx_global * panel_width
             fy_global += fy_global * panel_width
             fz_global += fz_global * panel_width
-
+        projected_area = 0
+        for wing in self.wings:
+            projected_area += wing.calculate_projected_area()
         ### Storing results in a dictionary
         results_dict = {}
         # Global wing aerodynamics
@@ -544,9 +545,9 @@ class WingAerodynamics:
         results_dict.update([("lift", lift_wing)])
         results_dict.update([("drag", drag_wing)])
         results_dict.update([("side", side_wing)])
-        results_dict.update([("cl", lift_wing / (q_inf * area_all_panels))])
-        results_dict.update([("cd", drag_wing / (q_inf * area_all_panels))])
-        results_dict.update([("cs", side_wing / (q_inf * area_all_panels))])
+        results_dict.update([("cl", lift_wing / (q_inf * projected_area))])
+        results_dict.update([("cd", drag_wing / (q_inf * projected_area))])
+        results_dict.update([("cs", side_wing / (q_inf * projected_area))])
         # Local panel aerodynamics
         results_dict.update([("cl_distribution", cl_prescribed_va_list)])
         results_dict.update([("cd_distribution", cd_prescribed_va_list)])
@@ -555,9 +556,9 @@ class WingAerodynamics:
         results_dict.update([("Ftotal_distribution", ftotal_prescribed_va_list)])
 
         # Additional info
-        results_dict.update([("cfz", fz_global / (q_inf * area_all_panels))])
-        results_dict.update([("cfx", fx_global / (q_inf * area_all_panels))])
-        results_dict.update([("cfy", fy_global / (q_inf * area_all_panels))])
+        results_dict.update([("cfz", fz_global / (q_inf * projected_area))])
+        results_dict.update([("cfx", fx_global / (q_inf * projected_area))])
+        results_dict.update([("cfy", fy_global / (q_inf * projected_area))])
         results_dict.update([("alpha_at_ac", alpha_corrected)])
         results_dict.update([("alpha_uncorrected", alpha_uncorrected)])
         results_dict.update([("gamma_distribution", gamma_new)])
@@ -570,6 +571,7 @@ class WingAerodynamics:
         logging.debug(f"drag:{drag_wing}")
         logging.debug(f"side:{side_wing}")
         logging.debug(f"Area: {area_all_panels}")
+        logging.debug(f"Projected Area: {projected_area}")
 
         return results_dict
 

tests/WingAerodynamics/test_calculate_results_against_output_results.py
@@ -18,6 +18,7 @@ from tests.utils import (
     generate_coordinates_rect_wing,
     generate_coordinates_curved_wing,
     flip_created_coord_in_pairs,
+    calculate_projected_area,
 )
 import tests.thesis_functions_oriol_cayon as thesis_functions
 
@@ -125,10 +126,7 @@ def test_calculate_results():
         {"tangential": panel.y_airf, "normal": panel.x_airf}
         for panel in wing_aero.panels
     ]
-    Atot = sum(
-        panel.chord * np.linalg.norm(panel.width * panel.z_airf)
-        for panel in wing_aero.panels
-    )
+    Atot = calculate_projected_area(coord)
 
     # Calculate results using the reference function
     F_rel_ref, F_gl_ref, Ltot_ref, Dtot_ref, CL_ref, CD_ref, CS_ref = (

tests/verification_cases/curved_wing/test_curved_wing.py
@@ -30,7 +30,6 @@ def get_curved_case_params():
     wing_type = "curved"
     Umag = 20
     AR = span / max_chord
-    Atot = 16
     R = 4.673
     theta = 45 * np.pi / 180
 
@@ -45,7 +44,7 @@ def get_curved_case_params():
         r"./polars/clarky_maneia.csv",
         delimiter=",",
     )
-
+    Atot = 14.40679
     case_parameters = [
         coord_input_params,
         aoas,

tests/verification_cases/v3/test_v3.py
@@ -25,10 +25,9 @@ def get_v3_case_params():
 
     wing_type = "LEI_kite"
     dist = "lin"
-    N_split = 4
+    N_split = 3
     aoas = np.arange(-4, 24, 2)
     Umag = 22
-    Atot = 19.753
     # convergence criteria
     max_iterations = 1500
     allowed_error = 1e-5
@@ -48,8 +47,7 @@ def get_v3_case_params():
     # Camber for each section (ct in my case)
     camber = 0.095
 
-    # Number of splits per panel
-    N_split = 5
+
     # Refine structrural mesh into more panels
     coord = thesis_functions.refine_LEI_mesh(coord, N - 1, N_split)
     N = int(len(coord) / 2)  # Number of section after refining the mesh
@@ -66,6 +64,8 @@ def get_v3_case_params():
         data_airf[j, 1] = Cl
         data_airf[j, 2] = Cd
         data_airf[j, 3] = Cm
+
+    Atot = test_utils.calculate_projected_area(coord)
     coord_input_params = [coord,LE_thicc,camber]
     case_parameters = [
         coord_input_params,
@@ -115,36 +115,31 @@ def test_v3():
     assert np.allclose(CL_VSM, CL_VSM_new, atol=4e-2)
     assert np.allclose(CD_VSM, CD_VSM_new, atol=4e-3)
 
-    # checking the LLT to be close to the VSM, with HIGHER tolerance
-    tol_llt_to_vsm_CL = 2e-1
-    tol_llt_to_vsm_CD = 4e-2
-    assert np.allclose(CL_LLT, CL_VSM, atol=tol_llt_to_vsm_CL)
-    assert np.allclose(CD_LLT, CD_VSM, atol=tol_llt_to_vsm_CD)
-    assert np.allclose(CL_LLT_new, CL_VSM_new, atol=tol_llt_to_vsm_CL)
-    assert np.allclose(CD_LLT_new, CD_VSM_new, atol=tol_llt_to_vsm_CD)
+    # comparing solution
+    CL_struts = np.loadtxt("./CFD_data/RANS_CL_alpha_struts.csv", delimiter=",")
+    CD_struts = np.loadtxt("./CFD_data/RANS_CD_alpha_struts.csv", delimiter=",")
 
-    polars_CFD = np.loadtxt("./CFD_DATA/CFD_data.csv", delimiter=",")
-    alpha_CFD = polars_CFD[:, 0]
-    CL_CFD = polars_CFD[:, 1]
-    CD_CFD = polars_CFD[:, 2]
-    # interpolating Maneia results to match the alphas
-    CL_maneia_at_new_alphas = []
-    CD_maneia_at_new_alphas = []
+    CL_CFD = CL_struts[:, 1]
+    alpha_CFD = CL_struts[:, 0]
+    CD_CFD = np.interp(alpha_CFD, CD_struts[:, 0], CD_struts[:, 1])
+    
+    CL_CFD_at_new_alphas = []
+    CD_CFD_at_new_alphas = []
     for alpha in aoas:
         alpha = np.rad2deg(alpha)
-        CL_maneia_at_new_alphas.append(np.interp(alpha, alpha_CFD, CL_CFD))
-        CD_maneia_at_new_alphas.append(np.interp(alpha, alpha_CFD, CD_CFD))
+        CL_CFD_at_new_alphas.append(np.interp(alpha, alpha_CFD, CL_CFD))
+        CD_CFD_at_new_alphas.append(np.interp(alpha, alpha_CFD, CD_CFD))
 
     # checking new VSM close to Maneia
-    assert np.allclose(CL_maneia_at_new_alphas, CL_VSM_new, atol=1e-1)
-    assert np.allclose(CD_maneia_at_new_alphas, CD_VSM_new, atol=1e-2)
+    assert np.allclose(CL_CFD_at_new_alphas, CL_VSM_new, atol=1e-1)
+    assert np.allclose(CD_CFD_at_new_alphas, CD_VSM_new, atol=1e-2)
 
 
 if __name__ == "__main__":
 
     case_params = get_v3_case_params()
 
-    aoas = np.deg2rad(np.arange(0,1, 1))
+    aoas = np.deg2rad(np.arange(0,20, 2))
     case_params[1] = aoas
     # comparing solution
     CL_struts = np.loadtxt("./CFD_data/RANS_CL_alpha_struts.csv", delimiter=",")